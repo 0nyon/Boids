3eda5465be04292616e15ba0da8045cdd2e93765
Yon <yh0542797120@gmail.com>
2026-06-06 16:36:54 +0300

    added all rules, the boids still have some weird behavior, i still need to add bounds checking and fix the texure thing

--- code/boidLogic.go
+++ code/boidLogic.go
@@ -7,8 +7,12 @@ import (
 )
 
 // ! all constants here
-const boidAmnt uint = 20
-const boidSightRange float32 = 20
+const boidAmnt uint = 50
+const boidSightRange float32 = 50
+const tooCloseRange float32 = 30
+const cohesionSteeringMult float32 = 0.15 //! has to be between 0-1
+const alignmentSteeringMult float32 = 0.4 //! has to be between 0-1
+const seperationSteeringMult float32 = 1  //! has to be between 0-1
 
 type boid struct {
 	posVec      rl.Vector2
@@ -36,25 +40,35 @@ func createAllBoids(tex *rl.Texture2D) []boid {
 	return res
 }
 
-func applySeperationToAllBoids(boids []boid) {
-	for i := range boids {
-		boids[i].applySeperationToBoid(boids[i].getAllboidsInSightRange(boids))
-	}
-}
-
 // * this function can take the slice of all boids, and one boid inside it
-func (this *boid) applySeperationToBoid(boidsInRange []boid) {
-	for _, b := range boidsInRange {
-		this.velocityVec = this.velocityVec.Add(this.velocityVec.Add(b.velocityVec.Subtract(this.velocityVec)))
+func (this *boid) applySeperationToBoid(boidsInRange []boid) rl.Vector2 {
+	if len(boidsInRange) == 0 {
+		return rl.Vector2{X: 0, Y: 0}
 	}
 
-	this.velocityVec = this.velocityVec.Normalize()
+	avgVec := rl.Vector2{X: 0, Y: 0}
+	for i := range boidsInRange {
+		b := boidsInRange[i]
+		avgVec = avgVec.Add(this.posVec.Subtract(b.posVec))
+	}
+
+	avgVec.X /= float32(len(boidsInRange))
+	avgVec.Y /= float32(len(boidsInRange))
+
+	desiredVel := avgVec.Normalize()
+
+	steeringVec := desiredVel.Subtract(this.velocityVec)
+	steeringVec.X *= seperationSteeringMult
+	steeringVec.Y *= seperationSteeringMult
+
+	return steeringVec
 }
 
 // * this function can take the slice of all boids, and one boid inside it
 func (this *boid) getAllboidsInSightRange(allBoids []boid) []boid {
 	res := make([]boid, 0)
-	for _, b := range allBoids {
+	for i := range allBoids {
+		b := allBoids[i]
 		if *this == b {
 			continue
 		}
@@ -65,12 +79,82 @@ func (this *boid) getAllboidsInSightRange(allBoids []boid) []boid {
 	return res
 }
 
+func (this *boid) getAllboidsInTooCloseRange(allBoids []boid) []boid {
+	res := make([]boid, 0)
+	for i := range allBoids {
+		b := allBoids[i]
+		if *this == b {
+			continue
+		}
+		if this.posVec.Distance(b.posVec) <= tooCloseRange {
+			res = append(res, b)
+		}
+	}
+	return res
+}
+
 func (b *boid) getLookingAngle() float32 {
 	return 360 * (b.velocityVec.Angle(rl.Vector2{X: 1, Y: 0}))
 }
 
+func (b *boid) applyAlignmentToBoid(visableBoids []boid) rl.Vector2 {
+	if len(visableBoids) == 0 {
+		return rl.Vector2{X: 0, Y: 0}
+	}
+	sum := rl.Vector2{X: 0, Y: 0}
+	for i := range visableBoids {
+		visable := visableBoids[i]
+		sum = sum.Add(visable.velocityVec)
+	}
+
+	sum.X /= float32(len(visableBoids))
+	sum.Y /= float32(len(visableBoids))
+
+	steeringVec := sum.Subtract(b.velocityVec)
+	steeringVec.X *= alignmentSteeringMult
+	steeringVec.Y *= alignmentSteeringMult
+
+	return steeringVec
+}
+
+func (b *boid) applyCohesionToBoid(visableBoids []boid) rl.Vector2 {
+	if len(visableBoids) == 0 {
+		return rl.Vector2{X: 0, Y: 0}
+	}
+
+	avgPos := rl.Vector2{X: 0, Y: 0}
+	for i := range visableBoids {
+		visable := visableBoids[i]
+
+		avgPos = avgPos.Add(visable.posVec)
+	}
+
+	avgPos.X /= float32(len(visableBoids))
+	avgPos.Y /= float32(len(visableBoids))
+
+	thisToAvgPos := avgPos.Subtract(b.posVec).Normalize()
+	steeringVec := thisToAvgPos.Subtract(b.velocityVec)
+
+	steeringVec.X *= cohesionSteeringMult
+	steeringVec.Y *= cohesionSteeringMult
+
+	return steeringVec
+}
+
 func applyAllRules(boids []boid) {
-	//! fun thing about go, a for range loop that refers to a value like so for _, v:= range ...
-	//! actually makes a copy and doesn't mutate the refernces, keep that in mind
-	applySeperationToAllBoids(boids)
+	//! fun thing about go, a for range loop that refers to a value, so: for _, v:= range ...
+	//! actually makes a copy (of v) and doesn't mutate the refernces, keep that in mind
+	steeringForEachBoid := make([]rl.Vector2, len(boids))
+
+	for i := range boids {
+		b := &boids[i]
+		steeringForEachBoid[i] = b.applySeperationToBoid(boids[i].getAllboidsInTooCloseRange(boids))
+		steeringForEachBoid[i] = steeringForEachBoid[i].Add(b.applyAlignmentToBoid(boids[i].getAllboidsInSightRange(boids)))
+		steeringForEachBoid[i] = steeringForEachBoid[i].Add(b.applyCohesionToBoid(boids[i].getAllboidsInSightRange(boids)))
+	}
+
+	for i := range boids {
+		b := &boids[i]
+		b.velocityVec = steeringForEachBoid[i].Add(b.velocityVec).Normalize()
+	}
 }
